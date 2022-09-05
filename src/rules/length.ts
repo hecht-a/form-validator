379b19e885a081a0c863b6ff0344d773ac4903ab
HECHT Axel <axel.hct@gmail.com>
2022-09-05 23:46:47 +0200

    chore: remove useless files

--- src/rules/length.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string} data
- * @param {number} length
- * @return {boolean}
- */
-export function length(data: string, length: number): boolean {
-	return data.length >= length
-}
--- src/rules/required.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @param {string} data
- * @return {boolean}
- */
-export function required(data: string): boolean {
-	return data === "" || data === null || data === undefined || data === " "
-}
