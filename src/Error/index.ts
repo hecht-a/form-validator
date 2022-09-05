5d8fc3f2b6b5e386393d48b1ad71db99515c020c
HECHT Axel <axel.hct@gmail.com>
2022-09-05 23:47:37 +0200

    chore: fix code style with eslint

--- src/Error/index.ts
+++ src/Error/index.ts
@@ -1,17 +1,16 @@
 export class Error {
-    constructor(private _field: string, private _error: string) {
-    }
+	constructor(private _field: string, private _error: string) {
+	}
 
-    public get field() {
-        return this._field
-    }
+	public get field(): string {
+		return this._field;
+	}
 
-    public get error() {
-        return this._error
-    }
+	public get error(): string {
+		return this._error;
+	}
 
-    public toString() {
-        return `${this.error.replace('{champ}', this.field)}`
-    }
-
+	public toString(): string {
+		return `${this.error.replace("{champ}", this.field)}`;
+	}
 }
--- src/Validator/index.ts
+++ src/Validator/index.ts
@@ -1,108 +1,115 @@
-import {CallableRule, Rules} from "../types"
-import {Error} from '../Error'
+import { CallableRule, Rules } from "../types";
+import { Error } from "../Error";
 
 export class Validator {
-    private _rules: Rules = {}
-    private _rulesMapping: Record<string, string[]> = {}
-    private _errors: Record<string, string[]> = {};
-    private _errorMessages: Record<string, string> = {};
+	private _rules: Rules = {};
 
-    constructor(private form: FormData) {
-    }
+	private _rulesMapping: Record<string, string[]> = {};
 
-    /**
+	private _errors: Record<string, string[]> = {};
+
+	private _errorMessages: Record<string, string> = {};
+
+	constructor(private form: FormData) {
+	}
+
+	/**
      * Return the rules
      */
-    get rules() {
-        return this._rules
-    }
+	get rules(): Rules {
+		return this._rules;
+	}
 
-    /**
+	/**
      * Return the errors
      */
-    public get errors() {
-        return this._errors
-    }
+	public get errors(): Record<string, string[]> {
+		return this._errors;
+	}
 
-    /**
+	public get errorMessage(): Record<string, string> {
+		return this._errorMessages;
+	}
+
+	/**
      * Init error messages with an object
      * The key is the form field, the value is the message
      * @param {Record<string, string>} errorMessages
      */
-    public initErrorMessages(errorMessages: Record<string, string>) {
-        this._errorMessages = errorMessages
-    }
+	public initErrorMessages(errorMessages: Record<string, string>): void {
+		this._errorMessages = errorMessages;
+	}
 
-    /**
+	/**
      * Add a custom rule to the validator
      * @param {string} name
      * @param {CallableRule} rule
      * @param {unknown[]} optionalParams
      */
-    public addRule(name: string, rule: CallableRule, ...optionalParams: unknown[]) {
-        this._rules[name] = {cb: rule, optionalParams}
+	public addRule(name: string, rule: CallableRule, ...optionalParams: unknown[]): Validator {
+		this._rules[name] = { cb: rule, optionalParams };
 
-        return this
-    }
+		return this;
+	}
 
-    /**
+	/**
      * Add an error for the specified field
      * @param {string} field
      * @param {string} errorMessage
      */
-    public addError(field: string, errorMessage: string) {
-        if(!this._errors[field] || this._errors[field].length < 0) {
-            this._errors[field] = []
-        }
-        this._errors[field].push(new Error(field, errorMessage).toString())
-    }
+	public addError(field: string, errorMessage: string): void {
+		if (!this._errors[field] || this._errors[field].length < 0) {
+			this._errors[field] = [];
+		}
+		this._errors[field].push(new Error(field, errorMessage).toString());
+	}
 
-    /**
+	/**
      * @param {string} cbName
      * @param {string} field
      * @param {string} fieldValue
      * @private
      */
-    private runValidator(cbName: string, field: string, fieldValue: any): void {
-        const rule = this.rules[cbName]
+	private runValidator(cbName: string, field: string, fieldValue: string): void {
+		const rule = this.rules[cbName];
 
-        if(rule.cb(fieldValue, ...rule.optionalParams ?? [])) {
-            this.addError(field, this._errorMessages[cbName])
-        }
-    }
+		if (rule.cb(fieldValue, ...rule.optionalParams ?? [])) {
+			this.addError(field, this._errorMessages[cbName]);
+		}
+	}
 
-    /**
+	/**
      * Check the form validity
      */
-    public validate(): void {
-        for (const field in this._rulesMapping) {
-            this._rulesMapping[field].forEach((cbName) => {
-                const fieldValue = this.form.get(field)
+	public validate(): void {
+		for (const field in this._rulesMapping) {
+			this._rulesMapping[field].forEach((cbName) => {
+				const fieldValue = this.form.get(field);
 
-                this.runValidator(cbName, field, fieldValue)
-            })
-        }
-    }
+				this.runValidator(cbName, field, fieldValue!.toString());
+			});
+		}
+	}
 
-    /**
+	/**
      * Set rule for a form field
      * @param {string} input
      * @param {string|string[]} rule
      */
-    public setRules(input: string, rule: string | string[]): void {
-        if (Array.isArray(rule)) {
-            return rule.forEach((r) => {
-                this.setRules(input, r)
-            })
-        }
+	public setRules(input: string, rule: string | string[]): void {
+		if (Array.isArray(rule)) {
+			return rule.forEach((r) => {
+				this.setRules(input, r);
+			});
+		}
 
-        const rulesField = this._rulesMapping[input]
+		const rulesField = this._rulesMapping[input];
 
-        if (rulesField) {
-            rulesField.push(rule)
-            return
-        }
+		if (rulesField) {
+			rulesField.push(rule);
+			return;
+		}
 
-        this._rulesMapping[input] = [rule]
-    }
+		this._rulesMapping[input] = [rule];
+	}
 }
